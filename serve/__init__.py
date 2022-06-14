9e7b47a652cb9c3eda013bfa6c2c4681fc204cf9
zhichaoGuo <cn.guozhichao@gmail.com>
2022-06-14 14:58:21 +0800

    删除固定数据库，改为每次创建数据库，新增requirements.txt

--- serve/__init__.py
+++ serve/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import subprocess
 from logging.handlers import TimedRotatingFileHandler
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -23,14 +24,15 @@ app.secret_key = 'Htek20180905'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/data.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-
+if os.path.exists(os.path.join(root_dir,'database')) is False:
+    os.mkdir(os.path.join(root_dir,'database'))
 from .DataBase import *
-
 db.create_all()
+
 loginManager = LoginManager(app)
 loginManager.session_protection = "strong"
 loginManager.login_view = 'index.login'
 loginManager.login_message = u'lic_maker平台必须登录，请登录您的平台账号！'
 
-# subprocess.run(['sudo','chmod','777','lic_maker'], cwd=root_dir, timeout=10)
-# subprocess.run(['sudo','chmod','777','zpipe'], cwd=root_dir, timeout=10)
+subprocess.run(['sudo','chmod','777','lic_maker'], cwd=root_dir, timeout=10)
+subprocess.run(['sudo','chmod','777','zpipe'], cwd=root_dir, timeout=10)
